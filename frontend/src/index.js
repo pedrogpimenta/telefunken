70db9b6470dd73c652778eb8039ec442871ba858
Pedro Pimenta <pedro@pimenta.co>
2020-03-28 02:05:36 +0100

    Add React Router and configure Routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 import './index.css';
-import App from './App';
+import Welcome from './components/Welcome';
+import App from './components/App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/game">
+          <App />
+        </Route>
+        <Route path="/">
+          <Welcome />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 serviceWorker.unregister();
