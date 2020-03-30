908a0a44b61b14aff48b21c31f3a1e65326d88e8
Pedro Pimenta <pedro@pimenta.co>
2020-03-30 18:14:16 +0200

    Add react build

---
+++ frontend/build/precache-manifest.eab5a0812034cda3b610f8ec5e80603f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad164c0b399351747300eaf3e724360d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41377e24f1a853c3e981",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fd821ffdf801d46ebd48",
+    "url": "/static/js/2.3aeea872.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.3aeea872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41377e24f1a853c3e981",
+    "url": "/static/js/main.35f77c78.chunk.js"
+  },
+  {
+    "revision": "263b00570e381abb75db",
+    "url": "/static/js/runtime-main.45aea150.js"
+  }
+]);
