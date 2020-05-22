993acef1b58d6583e71df6ec7d76839571629c1a
Pedro Pimenta <pedro@pimenta.co>
2020-05-22 16:11:43 +0200

    Update build

---
+++ frontend/build/precache-manifest.eb0aea4ec201755e6c025d3690aac736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9e69387b0cb5b99260f572f73805b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eff5f9055b487d56109f",
+    "url": "/static/css/main.86347b1c.chunk.css"
+  },
+  {
+    "revision": "c7e0057b80bb821d130b",
+    "url": "/static/js/2.7816ef1b.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.7816ef1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff5f9055b487d56109f",
+    "url": "/static/js/main.929ae172.chunk.js"
+  },
+  {
+    "revision": "43d9818bc523056baac5",
+    "url": "/static/js/runtime-main.84571290.js"
+  }
+]);
